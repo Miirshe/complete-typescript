87bf529b7e843a85f9323c03d3c9e4d3380edda7
Miirshe <miirshe@gmail.com>
2023-11-25 11:44:37 +0300

    index signature , interfaces , assertion keyOf and typeOf

--- lesson05/build/js/main.js
+++ lesson05/build/js/main.js
@@ -49,3 +49,56 @@ function person_data(person) {
     }
 }
 person_data(personData);
+class StudentInfo {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+class Student_marks extends StudentInfo {
+    constructor(name, age, gender, somali, english, math) {
+        super(name, age, gender);
+        this.somali = somali;
+        this.english = english;
+        this.math = math;
+    }
+    static calculateMarks(somali, english, math) {
+        let grade;
+        (function (grade) {
+            grade["A"] = "A";
+            grade["B"] = "B";
+            grade["C"] = "C";
+            grade["D"] = "D";
+            grade["F"] = "F";
+        })(grade || (grade = {}));
+        let subjectMarks = {
+            somali: somali,
+            english: english,
+            math: math
+        };
+        let total = 0;
+        for (let key in subjectMarks) {
+            total += subjectMarks[key];
+        }
+        let average = total / [subjectMarks].length;
+        if (average > 90 && average <= 100) {
+            console.log(`${total}\n${average}\n${grade.A}`);
+        }
+        else if (average > 80 && average <= 90) {
+            console.log(`${total}\n${average}\n${grade.B}`);
+        }
+        else if (average > 70 && average <= 80) {
+            console.log(`${total}\n${average}\n${grade.C}`);
+        }
+        else if (average > 60 && average <= 70) {
+            console.log(`${total}\n${average}\n${grade.D}`);
+        }
+        else if (average > 50 && average <= 60) {
+            console.log(`${total}\n${average}\n${grade.F}`);
+        }
+    }
+}
+let studentResultExam = new Student_marks('miirshe', 23, 'male', 100, 100, 98);
+console.log(studentResultExam);
+Student_marks.calculateMarks(100, 100, 98);
--- lesson05/src/main.ts
+++ lesson05/src/main.ts
@@ -94,3 +94,100 @@ function person_data(person : Person):void{
 
 person_data(personData);
 
+
+interface studentObj{
+	[index:string] : string | number | null | undefined | boolean | void
+}
+
+class StudentInfo implements studentObj{
+	[index: string]: string | number | boolean | null | undefined;
+	name : string;
+	age : number;
+	gender : string;
+	constructor(
+		name : string , 
+		age: number , 
+		gender: string){
+		this.name = name;
+		this.age = age;
+		this.gender = gender
+	}
+
+}
+
+class Student_marks extends StudentInfo{
+	somali : number;
+	english : number;
+	math : number;
+
+	constructor(
+		name : string , 
+		age: number , 
+		gender: string,
+		somali : number, 
+		english : number,
+		math : number){
+		super(name , age , gender);
+		this.somali = somali;
+		this.english = english;
+		this.math = math
+	}
+
+    static calculateMarks(
+		somali : number, 
+		english : number,
+		math : number
+	):void {
+		enum grade {
+			A = "A",
+			B = "B",
+			C = "C",
+			D = "D",
+			F = "F"
+		}
+		interface marks{
+			somali : number,
+			english : number,
+			math : number
+		}
+		
+		let subjectMarks : marks = {
+			somali :somali,
+			english : english,
+			math : math
+		}
+		let total : number = 0 ;
+		for(let key in subjectMarks){
+			total += subjectMarks[key as keyof marks]
+		}
+
+		let average : number = total/[subjectMarks].length;
+
+		if(average > 90 && average <= 100){
+			console.log(`${total}\n${average}\n${grade.A}`);
+		}else if(average > 80 && average <= 90){
+			console.log(`${total}\n${average}\n${grade.B}`);
+		}else if(average > 70 && average <= 80){
+			console.log(`${total}\n${average}\n${grade.C}`);
+		}else if(average > 60 && average <= 70){
+			console.log(`${total}\n${average}\n${grade.D}`);
+		}else if(average > 50 && average <= 60){
+			console.log(`${total}\n${average}\n${grade.F}`);
+		}
+	}
+}
+
+let studentResultExam = new Student_marks(
+	'miirshe',
+	23,
+	'male',
+	100,
+	100,
+	98
+)
+
+console.log(studentResultExam);
+
+
+Student_marks.calculateMarks(100,100,98)
+
